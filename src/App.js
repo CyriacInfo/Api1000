06d23ca8c0b87923a260c72edf90d70c6c147328
cyriacinfo <cyriac.intesse@gmail.com>
2021-11-25 16:55:25 +0100

    Init api usefeect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 function App() {
-  return <div className="App">Hello la team !</div>;
+  const [playList, setPlayListList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLNxOe-buLm6cz8UQ-hyG1nm3RTNBUBv3K&key=AIzaSyD-FWZXxe1RVKRUZRouRY2zVM3a9u_MeXY&playListId=PLNxOe-buLm6cz8UQ-hyG1nm3RTNBUBv3K&maxResults=50`
+      )
+      .then((res) => setPlayListList(res.data))
+      .then(setIsLoading(!isLoading));
+  }, []);
+  console.log(playList);
+  return (
+    <div className="App">{isLoading ? "Hello la team !" : "...loading"}</div>
+  );
 }
 
 export default App;

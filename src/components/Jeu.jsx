a20ffe57f9f54d4c5e9b6affca2abaaff0e94eef
NoMorSorrow <ghislaindu28@hotmail.fr>
2021-11-25 23:55:47 +0100

    avancée composant jeu

--- src/components/Jeu.jsx
+++ src/components/Jeu.jsx
@@ -3,13 +3,27 @@ import "./Jeu.css";
 
 export default function Jeu() {
   return (
-    <div className="mario">
-      <div className="Timer"></div>
-      <div className="Indice"></div>
-      <div className="Carre"></div>
-      <h2>Artist</h2>
-      <input className="InputReponse" type="text"></input>
-      <button className="ButtonReponse" type="button"></button>
+    <div>
+      <div className="container_button">
+        <button className="Fermer_le_jeu" type="button">
+          X
+        </button>
+        <h2 className="Nom_fermer_le_jeu">Quitter</h2>
+      </div>
+      <div className="block_jeu">
+        <div className="info_jeu">
+          <div className="timer"></div>
+          <div className="points"></div>
+        </div>
+        <div className="container_jeu">
+          <div className="titre_jeu">Devine l'artiste</div>
+          <div className="carre1"></div>
+          <div className="carre2"></div>
+          <div className="carre3"></div>
+          <div className="carre4"></div>
+        </div>
+      </div>
+      <div className="block_reponse"></div>
     </div>
   );
 }

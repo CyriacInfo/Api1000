84977b8eaa0fca06b4841c1eef62844d5399e5ed
[LL] Mario Silva <mario_jj_silva@hotmail.com>
2021-11-25 19:33:15 +0100

    save jeu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import PagePrincipale from "./components/PagePrincipale";
+import Jeu from "./components/Jeu";
 
 
 function App() {
   return (
+    <div>
     <PagePrincipale />
-  )
+    <Jeu />
+    </div>
+  );
 }
 
 export default App;

8c75ff2c4df914b44bdc6a397f338d3b565fca27
wilder <Paco_fleuru@yahoo.fr>
2021-11-25 17:02:57 +0100

    Préparation merge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import "./App.css";
+
 function App() {
-  return <div className="App">Hello la team !</div>;
+  return <div>
+    <div className="HeadMain">
+      <img className="LogoMain" src="assets/Logo.png" alt="logo" />
+      <div className="ContainerSearch">
+        <img className="ContainerSearchImg" src="assets/search.png" alt="logo" />
+        <input className="RechargeBar" type="search" id="site-search" name="q"
+          aria-label="Search through site content" placeholder="Rechercher" />
+      </div>
+    </div>
+    <div className="NavButton">
+      <button className="ButtonMain"><img className="ButtonMainImg" src="assets/chevron-left.png" alt="logo" /></button>
+      <button className="ButtonMain"><img className="ButtonMainImg" src="assets/chevron-right.png" alt="logo" /></button>
+    </div>
+  </div>
 }
 
 export default App;
